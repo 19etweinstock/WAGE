2de8ef2ecd7d65548686beded01a1243e4894000
19etweinstock <ethangweinstock@gmail.com>
2021-11-12 16:57:19 -0500

    working on rewrite

---
+++ custom/Layers.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4/bin") 
+import tensorflow as tf
+
+import Quantize
+from functools import reduce
+
+def stride(_stride):
+    return [1, 1, _stride, _stride]
+
+def QE(x):
+    if Quantize.bitsE <= 16:
+        x = Quantize.E(x)
+    return x
+    
+def QA(x):
+    if Quantize.bitsA <= 16:
+        x = Quantize.A(x)
+    return x
+
+class qconv2d(tf.keras.layers.Layer):
+    def __init__(self, ksize, c_out, stride=1, padding='SAME',name='conv'):
+        super().__init__()
+        self.ksize = ksize
+        self.c_out = c_out
+        self.stride = stride
+        self.padding = padding
+        self.named = name
+        
+    def build(self, input_shape):
+        #input shape is NCHW:
+        self.kernel = self.add_weight("kernel", \
+                                        shape=[self.ksize, self.ksize, input_shape[1], self.c_out])
+
+    def call(self, inputs):
+        return tf.nn.conv2d(input=inputs, \
+                            filters=Quantize.W(self.kernel), \
+                            strides=stride(self.stride), \
+                            padding=self.padding, \
+                            data_format='NCHW', \
+                            name=self.named)
+
+class maxpool(tf.keras.layers.Layer):
+    def __init__(self, ksize=2, stride=1, padding='VALID'):
+        super().__init__()
+        self.ksize = ksize
+        self.stride = stride
+        self.padding = padding
+
+    def call(self, inputs):
+        return tf.nn.max_pool(  input=inputs,\
+                                ksize=self.ksize, \
+                                strides=stride(self.stride), \
+                                padding=self.padding, \
+                                data_format='NCHW')
+
+class qactivation(tf.keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+
+    def call(self, inputs):
+        x = tf.nn.relu(inputs)
+        x = QE(x)
+        x = QA(x)
+        return x
+
+class qfc(tf.keras.layers.Layer):
+    def __init__(self, c_out, name) -> None:
+        super().__init__()
+        self.c_out = c_out
+        self.named = name
+
+    def build(self, input_shape):
+        #input shape is NCHW:
+        self.kernel = self.add_weight("kernel", \
+                                        shape=[input_shape[1], self.c_out])
+    
+    def call(self, inputs):
+        return tf.matmul(inputs, Quantize.W(self.kernel))
+
+class reshape(tf.keras.layers.Layer):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def call(self, inputs):
+        shape = reduce(lambda inputs, y: inputs * y, inputs.get_shape().as_list()[1:])
+        x = tf.reshape(inputs, [-1, shape])
---
+++ custom/Model.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4/bin") 
+import tensorflow as tf
+
+import Layers
+
+class Lenet5(tf.keras.Model):
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv0 = Layers.qconv2d(5, 6, name='conv0')
+        self.pool0 = Layers.maxpool()
+        self.activation0 = Layers.qactivation()
+
+        self.conv1 = Layers.qconv2d(5, 8, name='conv1')
+        self.pool1 = Layers.maxpool()
+        self.activation1 = Layers.qactivation()
+
+        self.reshape=Layers.reshape()
+
+        self.fc0 = Layers.qfc(120, 'fc0')
+        self.activation2 = Layers.qactivation()
+        self.fc1 = Layers.qfc(84, 'fc1')
+        self.activation3 = Layers.qactivation()
+        self.fc2 = Layers.qfc(10, 'fc2')
+
+        self.QA = Layers.QA
+        self.QE = Layers.QE
+
+    def call(self, input, training=False):
+        x = self.conv0(input)
+        x = self.pool0(x)
+        x = self.activation0(x)
+
+        x = self.conv1(x)
+        x = self.pool1(x)
+        x = self.activation1(x)
+
+        x = self.reshape(x)
+
+        x = self.fc0(x)
+        x = self.activation2(x)
+        x = self.fc1(x)
+        x = self.activation3(x)
+        x = self.fc2(x)
+
+        x = self.QA(x)
+        x = self.QE(x)
+
+        return x
