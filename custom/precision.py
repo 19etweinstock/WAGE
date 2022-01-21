d3b4d0ef5fc1551654143a9bcac47a0e2558184f
19etweinstock <ethangweinstock@gmail.com>
2022-01-20 20:52:40 -0500

    working non quantized models

--- custom/precision.py
+++ custom/precision.py
@@ -12,6 +12,8 @@ x_train = np.expand_dims(x_train, 3)
 x_test = np.expand_dims(x_test, 3)
 y_train = y_train / 1.0
 y_test = y_test / 1.0
+y_train = tf.one_hot(y_train, 10)
+y_test = tf.one_hot(y_test, 10)
 
 Time = time.strftime('%Y-%m-%d %H%M', time.localtime())
 
@@ -28,31 +30,16 @@ print(Time)
 def sse(y_true, y_pred):
     return tf.math.reduce_sum(tf.square(y_true - y_pred))
 
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Conv2D(6,5, padding='valid', data_format='channels_first', use_bias=False),
-#     tf.keras.layers.MaxPool2D(strides=(2,2), data_format='channels_first'),
-#     tf.keras.layers.ReLU(),
-#     tf.keras.layers.Conv2D(8,5, padding='valid', data_format='channels_first', use_bias=False),
-#     tf.keras.layers.MaxPool2D(strides=(2,2), data_format='channels_first'),
-#     tf.keras.layers.ReLU(),
-#     tf.keras.layers.Reshape((-1,)),
-#     tf.keras.layers.Dense(120, use_bias=False),
-#     tf.keras.layers.ReLU(),
-#     tf.keras.layers.Dense(84, use_bias=False),
-#     tf.keras.layers.ReLU(),
-#     tf.keras.layers.Dense(10, use_bias=False),
-#     tf.keras.layers.Softmax()
-# ])
-
 import tensorflow.keras.models as models
 import tensorflow.keras.layers as layers
+import tensorflow.keras.losses as losses
 
 model = models.Sequential()
-model.add(layers.Conv2D(6, 5, activation='relu', input_shape=x_train.shape[1:]))
+model.add(layers.Conv2D(6, 5, input_shape=x_train.shape[1:]))
 model.add(layers.MaxPool2D(2))
 model.add(layers.Activation('relu'))
-model.add(layers.Conv2D(8, 5, activation='relu'))
-model.add(layers.AveragePooling2D(2))
+model.add(layers.Conv2D(8, 5))
+model.add(layers.MaxPool2D(2))
 model.add(layers.Activation('relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(120, activation='relu'))
@@ -61,11 +48,13 @@ model.add(layers.Dense(10, activation='softmax'))
 model.summary()
 
 
-model.compile(optimizer=tf.keras.optimizers.SGD(\
-              learning_rate=1.0),
-              loss=sse,
-              metrics=['accuracy'],
-              run_eagerly=False)
+# model.compile(optimizer=tf.keras.optimizers.SGD(\
+#               learning_rate=1.1),
+#               loss=losses.sparse_categorical_crossentropy,
+#               metrics=['accuracy'],
+#               run_eagerly=False)
+
+model.compile(optimizer='sgd', loss='mse', metrics=['accuracy'], run_eagerly=False)
 
 model.fit(x_train, y_train, epochs=40,batch_size=64, validation_data=(x_test, y_test))
 model.evaluate(x_test, y_test)
