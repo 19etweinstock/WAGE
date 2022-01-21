84aedf093f3af7707746d031ce00091470552ddd
19etweinstock <ethangweinstock@gmail.com>
2022-01-21 14:24:44 -0500

    working on integer lr

--- custom/Option.py
+++ custom/Option.py
@@ -1,16 +1,16 @@
 LR    = 1
-bitsW = 15  # bit width of weights
-bitsA = 15 # bit width of activations
-bitsG = 15  # bit width of gradients
-bitsE = 15 # bit width of errors
+bitsW = 1  # bit width of weights
+bitsA = 2 # bit width of activations
+bitsG = 8  # bit width of gradients
+bitsE = 8 # bit width of errors
 
-bitsR = 15  # bit width of randomizer
+bitsR = 8  # bit width of randomizer
 
-loops = 4
+loops = 8
 Epoch = 50
 batchSize = 50
 
-lr_schedule = [0, 1., 20, 0.7, 35, 0.2]
+lr_schedule = [0, 1., 20, 0.5, 35, 0.25]
 
 loadModel = None
-# loadModel = '2021-12-03 1640(MNIST 1288 8 [0, 1.0, 20, 0.7, 35, 0.2] 50 50)'
+# loadModel = '2022-01-20 2053(MNIST 1288 8 [0, 1.0, 20, 0.5, 35, 0.25] 50 50)'
